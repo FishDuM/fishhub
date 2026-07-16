82bbd912e44cc2120e3198c7e9707c96d4860f34
FishDuM <3057433102@qq.com>
2026-07-16 21:08:15 +0800

    微服务骨架搭建-通用模块

---
+++ fishhub-framework/fishhub-common/src/main/java/hk/ljx/framework/common/exception/BaseExceptionInterface.java
@@ -0,0 +1,10 @@
+package hk.ljx.framework.common.exception;
+
+public interface BaseExceptionInterface {
+
+    // 获取异常吗
+    String getErrorCode();
+
+    // 获取异常信息
+    String getErrorMessage();
+}
