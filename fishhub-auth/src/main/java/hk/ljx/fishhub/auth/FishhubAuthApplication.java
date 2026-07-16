fdfaddaa63b34e3b3fbe51310433d3012419d291
FishDuM <3057433102@qq.com>
2026-07-16 19:46:55 +0800

    微服务骨架搭建-认证模块

---
+++ fishhub-auth/src/main/java/hk/ljx/fishhub/auth/FishhubAuthApplication.java
@@ -0,0 +1,13 @@
+package hk.ljx.fishhub.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishhubAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishhubAuthApplication.class, args);
+    }
+
+}
---
+++ fishhub-auth/src/test/java/hk/ljx/fishhub/auth/FishhubAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package hk.ljx.fishhub.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishhubAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
