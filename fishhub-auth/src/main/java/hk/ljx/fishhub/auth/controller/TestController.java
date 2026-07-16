a6c88526f0450d303076f2a9b61b0f3147897e65
FishDuM <3057433102@qq.com>
2026-07-17 02:00:19 +0800

    添加全局异常捕获、接口参数校验

--- fishhub-auth/src/main/java/hk/ljx/fishhub/auth/controller/TestController.java
+++ fishhub-auth/src/main/java/hk/ljx/fishhub/auth/controller/TestController.java
@@ -2,7 +2,10 @@ package hk.ljx.fishhub.auth.controller;
 
 import hk.ljx.fishhub.framework.biz.operationlog.aspect.ApiOperationLog;
 import hk.ljx.framework.common.response.Response;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +15,12 @@ public class TestController {
     @GetMapping("/test")
     public Response<String> test(){
         return Response.success("test");
+    }
+
+    @PostMapping("/test2")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test2(@RequestBody @Validated User user) {
+        int i = 1 / 0;
+        return Response.success(user);
     }
 }
