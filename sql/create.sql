2f306e5387303e19770b8e3ff0d2d3367c727bbf
FishDuM <3057433102@qq.com>
2026-07-17 02:30:27 +0800

    整合 sa-token

--- sql/create.sql
+++ sql/create.sql
@@ -2,9 +2,22 @@ create database if not exists `fishhub`;
 
 use `fishhub`;
 
-create table if not exists `t_user` (
-                                        `id` bigint(20) unsigned not null auto_increment comment '主键',
-                                        `username` varchar(64) not null comment '用户名',
-                                        `create_time` datetime not null default current_timestamp comment '创建时间',
-                                        `update_time` datetime not null default current_timestamp on update current_timestamp comment '更新时间',
-                                        primary key (`id`) using btree)engine = innodb comment = '用户表';
+CREATE TABLE `t_user` (
+                          `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                          `fishhub_id` varchar(15) NOT NULL COMMENT 'fishhub 号(唯一凭证)',
+                          `password` varchar(64) DEFAULT NULL COMMENT '密码',
+                          `nickname` varchar(24) NOT NULL COMMENT '昵称',
+                          `avatar` varchar(120) DEFAULT NULL COMMENT '头像',
+                          `birthday` date DEFAULT NULL COMMENT '生日',
+                          `background_img` varchar(120) DEFAULT NULL COMMENT '背景图',
+                          `phone` varchar(11) NOT NULL COMMENT '手机号',
+                          `sex` tinyint DEFAULT '0' COMMENT '性别(0：女 1：男)',
+                          `status` tinyint NOT NULL DEFAULT '0' COMMENT '状态(0：启用 1：禁用)',
+                          `introduction` varchar(100) DEFAULT NULL COMMENT '个人简介',
+                          `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                          `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+                          `is_deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '逻辑删除(0：未删除 1：已删除)',
+                          PRIMARY KEY (`id`) USING BTREE,
+                          UNIQUE KEY `uk_fishhub_id` (`fishhub_id`),
+                          UNIQUE KEY `uk_phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
