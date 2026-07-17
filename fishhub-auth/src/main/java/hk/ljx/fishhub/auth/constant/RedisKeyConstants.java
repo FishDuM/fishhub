bac9190b8bab23f9d1bb10dd73a78378e8b908d8
FishDuM <3057433102@qq.com>
2026-07-17 18:40:27 +0800

    实现项目启动时自动同步角色-权限集合数据到 Redis 中

--- fishhub-auth/src/main/java/hk/ljx/fishhub/auth/constant/RedisKeyConstants.java
+++ fishhub-auth/src/main/java/hk/ljx/fishhub/auth/constant/RedisKeyConstants.java
@@ -35,4 +35,19 @@ public class RedisKeyConstants {
     public static String buildUserRoleKey(String phone) {
         return USER_ROLES_KEY_PREFIX + phone;
     }
+
+    /**
+     * 角色对应的权限集合 KEY 前缀
+     */
+    private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
+
+
+    /**
+     * 构建角色对应的权限集合 KEY
+     * @param roleId
+     * @return
+     */
+    public static String buildRolePermissionsKey(Long roleId) {
+        return ROLE_PERMISSIONS_KEY_PREFIX + roleId;
+    }
 }
--- fishhub-auth/src/main/java/hk/ljx/fishhub/auth/domain/mapper/RoleDOMapper.java
+++ fishhub-auth/src/main/java/hk/ljx/fishhub/auth/domain/mapper/RoleDOMapper.java
@@ -2,6 +2,8 @@ package hk.ljx.fishhub.auth.domain.mapper;
 
 import hk.ljx.fishhub.auth.domain.dataobject.RoleDO;
 
+import java.util.List;
+
 public interface RoleDOMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,10 @@ public interface RoleDOMapper {
     int updateByPrimaryKeySelective(RoleDO record);
 
     int updateByPrimaryKey(RoleDO record);
+
+    /**
+     * 查询所有启用的角色
+     * @return
+     */
+    List<RoleDO> selectEnabledList();
 }
