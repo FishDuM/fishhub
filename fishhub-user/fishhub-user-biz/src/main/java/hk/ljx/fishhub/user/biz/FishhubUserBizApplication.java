9603fab6e4a42f0b4dc96b460e15265ab4b39684
FishDuM <3057433102@qq.com>
2026-07-18 23:55:11 +0800

    用户微服务搭建

---
+++ fishhub-user/fishhub-user-biz/src/main/java/hk/ljx/fishhub/user/biz/FishhubUserBizApplication.java
@@ -0,0 +1,11 @@
+package hk.ljx.fishhub.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishhubUserBizApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(FishhubUserBizApplication.class, args);
+    }
+}
